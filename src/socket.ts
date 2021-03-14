afdc1d7f88433b36965a3e77cffb615428062fff
nostam <matsonho@gmail.com>
2021-03-14 20:34:17 +0100

    small update on socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,22 @@
 import socketio, { Socket } from "socket.io";
 import { Server } from "http";
-import { Express } from "express";
 import logger from "./shared/Logger";
 
 export default function SocketServer(server: Server) {
   const io = socketio(server);
   io.on("connection", (socket: Socket) => {
-    logger.info("connection established: " + socket.id);
+    logger.imp("connection established: " + socket.id);
+    socket.on("joinRoom", async (data) => {
+      try {
+        socket.join(data.room);
+        logger.info(`${socket.id} joined ${data.room}`);
+        const msgToRoomMembers = {};
+        socket.broadcast.to(data.room).emit("message", msgToRoomMembers);
+        const roomData = { room: "", users: [] };
+        io.to(data.room).emit("roomData", roomData);
+      } catch (error) {
+        logger.err(error);
+      }
+    });
   });
 }
