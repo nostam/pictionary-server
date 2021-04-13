01f7f4efbba696b899a8118c0ff87dd7cfa67e5e
nostam <matsonho@gmail.com>
2021-04-13 20:42:44 +0200

    Add dynamic cookies cfg

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,25 +1,30 @@
 import { CookieOptions } from "express";
-
+import { env } from "../shared/functions";
 export const JWT_SECRET = process.env.JWT_SECRET!;
 export const REFRESH_JWT_SECRET = process.env.REFRESH_JWT_SECRET!;
 
 // ovewrite is true by default
+
+const DynCookiesCfg = () => {
+  return {
+    sameSite: env("env") === "development" ? true : "none",
+    secure: env("env") === "development" ? false : true,
+  };
+};
+
 export const accessTokenOptions: CookieOptions = {
   httpOnly: true,
   path: "/",
-  sameSite: "none",
-  secure: true,
   maxAge: 900000,
+  ...DynCookiesCfg,
 };
 export const refreshTokenOptions: CookieOptions = {
   httpOnly: true,
   path: "/users/refreshToken",
-  sameSite: "none",
-  secure: true,
   maxAge: 604800000,
+  ...DynCookiesCfg,
 };
 export const rmbOptions: CookieOptions = {
   path: "/",
-  sameSite: "none",
-  secure: true,
+  ...DynCookiesCfg,
 };
